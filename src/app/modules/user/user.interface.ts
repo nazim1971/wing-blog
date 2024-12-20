3429f82ed4edbaf0e75bbcec6aa96e6333d7a53c
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-21 01:15:38 +0600

    create user modules

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,7 +1,11 @@
+import { USER_ROLE } from "./user.constants";
+
+export type TUserRole = (typeof USER_ROLE)[keyof typeof USER_ROLE];
+
 export interface TUser {
     name: string;
     email: string;
     password: string;
-    role: "admin" | "user";
+    role: TUserRole
     isBlocked: boolean;
 }
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,4 +1,36 @@
-import { Schema } from "mongoose";
-import { TUser } from "./user.interface";
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
 
-const userSchema = new Schema<TUser>
+const userSchema = new Schema<TUser>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        select: false,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true
+});
+
+
+export const user = model<TUser>('User', userSchema)
